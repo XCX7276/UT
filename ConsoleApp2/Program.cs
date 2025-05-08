debbe189d4b5d7d775bc0b91b553d5b60d4a2ab8
XCX7276 <115113822+XCX7276@users.noreply.github.com>
2025-05-08 18:20:08 +0300

    +

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] mass = { 4, 0, 3, 19, 492, -10, 1 }; 
+            Console.WriteLine(MinSum(mass));
+            Console.WriteLine();
+
+
+            int[] nums = new int[4];
+            Console.WriteLine("hash "+ nums.GetHashCode());
+            foreach (var item in nums)
+            { 
+                Console.Write(item + " " + item.GetType() + " " + item.GetTypeCode() + " " +item.GetHashCode() +" "   );
+            }
+            Console.WriteLine();
+
+
+            Console.WriteLine();
+            Console.ReadLine();
+        } 
+        static public int? MinSum(int[] array)
+        {
+            if (array.Length < 2) return null; 
+            Array.Sort(array); 
+            return array[0] + array[1];
+        }
+        static public double? MinSum(double[] array)
+        {
+            if (array.Length < 2) return null;
+            Array.Sort(array);
+            return array[0] + array[1];
+        }
+        static public int? MinSum(string[] array)
+        { 
+            return null;
+        }
+        static public int? MinSum(char[] array)
+        {
+            return null;
+        }
+    }
+}
+/*
+ Напишите функцию, на вход которой приходит массив чисел. Функция возвращает сумму двух минимальных элементов массива.
+Например, если дан массив [4, 0, 3, 19, 492, -10, 1], то результатом будет -10, потому что два минимальных числа -10 и 0, а их сумма -10.
+Напишите минимум 3 модульных теста на эту функцию.
+HINT: учти, что массив может быть пустым, или без цифр или состоять из 100 млн. элементов, поэтому надо учесть разные граничные условия.
+Результат тестового выложи в виде ссылки на репозиторий с исходным кодом на github.com
+ 
+ */
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            int[] mass = { 4, 0, 3, 19, 492, -10, 1 };
+            Assert.AreEqual(ConsoleApp2.Program.MinSum(mass), -10);
+        } 
+        [TestMethod]
+        public void TestMethod3()
+        {
+            double[] mass = { 4,  0, 3, 19, 492, -10, 1 };
+            Assert.AreEqual(ConsoleApp2.Program.MinSum(mass), -10);
+        }
+        [TestMethod]
+        public void TestMethod4()
+        {
+            char[] mass = { 'f' };
+            Assert.AreEqual(ConsoleApp2.Program.MinSum(mass), null);
+        }
+        [TestMethod]
+        public void TestMethod5()
+        {
+            string[] mass = { "ddd", "ddd", "ddd" };
+            Assert.AreEqual(ConsoleApp2.Program.MinSum(mass), null);
+        }
+        [TestMethod]
+        public void TestMethod6()
+        {
+
+            int[] nums = new int[4];
+            Assert.AreEqual(ConsoleApp2.Program.MinSum(nums), 0);
+        }
+        [TestMethod]
+        public void TestMethod7()
+        {
+            int[] mass = { 1 };
+            Assert.AreEqual(ConsoleApp2.Program.MinSum(mass), null);
+        } 
+    }
+}
